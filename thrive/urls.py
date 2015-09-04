99970887091a223885fb4fcc110f2c35395cb79f
Cgeist7 <cgeist7@gmail.com>
2015-09-04 07:12:13 -0400

    static update

--- thrive/urls.py
+++ thrive/urls.py
@@ -18,4 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^thrive_core/', include('thrive_core.urls')),
 ]
--- thrive_core/models.py
+++ thrive_core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
